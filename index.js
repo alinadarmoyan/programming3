5c5c4017f4837c362f548ef14754e74794abbbc1
alinadarmoyan <96479189+alinadarmoyan@users.noreply.github.com>
2022-03-18 17:02:31 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,54 @@
+// var os = require("os");
+// var message = "The platform is ";
+
+// function main(){
+//    console.log(message + os.platform());
+// }
+// main();
+
+// var express = require("express");
+// var app = express();
+
+// app.get("/", function(req, res){
+//    res.send("Hello world");
+// });
+
+// app.listen(3000, function(){
+//    console.log("Example is running on port 3000");
+// });
+
+// var express = require("express");
+// var app = express();
+
+// app.get("/", function(req, res){
+//    res.send("<h1>Hello hey</h1>");
+// });
+// app.get("/name/:name", function(req, res){
+//    var name = req.params.name;
+//    res.send("<h1>Hello " + name +"</h1>");
+// });
+// app.listen(3000, function(){
+//    console.log("Example is running on port 3000");
+// });
+
+// app.get("/search/:name", function(req, res){
+//     var name = req.params.name;
+//     res.redirect('https://google.com/search?q=' + name)
+//  });
+
+// app.listen(3000, function(){
+//    console.log("Example is running on port 3000");
+// });
+ 
+var express = require("express");
+var app = express();
+
+app.use(express.static("programming3/Gameoflife2"));
+
+app.get("/", function(req, res){
+   res.redirect("index.html");
+});
+
+app.listen(3000, function(){
+   console.log("Example is running on port 3000");
+});
---
+++ programming3/Gameoflife2/Predator.js
@@ -0,0 +1,100 @@
+class Predator extends LivingCreature{
+    constructor(x, y) {
+        super(x, y)
+        this.energy = 50
+        
+    }
+
+    getNewCoordinates() {
+        this.directions = [
+            [this.x - 1, this.y - 1],
+            [this.x    , this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y    ],
+            [this.x + 1, this.y    ],
+            [this.x - 1, this.y + 1],
+            [this.x    , this.y + 1],
+            [this.x + 1, this.y + 1]
+        ];
+    }
+
+    chooseCell(char) {
+        this.getNewCoordinates()
+        return super.chooseCell(char)
+    }
+
+    mul() {
+        let found = this.chooseCell(0)
+        let exact = random(found)
+        if (exact && this.energy >= 100) {
+            let x = exact[0]
+            let y = exact[1]
+
+            let eater = new Predator(x, y)
+            matrix[y][x] = 3
+            predatorArr.push(eater)
+
+            this.energy = 50
+        }
+    }
+
+    eat() {
+        let found = this.chooseCell(2)
+        let exact = random(found)
+        if (exact) {
+            this.energy += 5
+            let x = exact[0]
+            let y = exact[1]
+            for (let i = 0; i < grassEaterArr.length; i++) {
+                if (grassEaterArr[i].x == x && grassEaterArr[i].y == y) {
+                    grassEaterArr.splice(i, 1)
+                }
+            }
+            matrix[y][x] = 3
+            matrix[this.y][this.x] = 0
+
+            this.x = x
+            this.y = y
+            if (this.energy >= 100) {
+                this.mul()
+            }
+        }
+        else {
+            this.move()
+        }
+    }
+
+    move() {
+        let found = this.chooseCell(0)
+        let exact = random(found)
+        if (exact) {
+            let x = exact[0]
+            let y = exact[1]
+            matrix[y][x] = 3
+            matrix[this.y][this.x] = 0
+
+            this.x = x
+            this.y = y
+            this.energy --
+            if (this.energy <= 0) {
+                this.die()
+            }
+        }
+        else {
+            this.energy --
+            if (this.energy <= 0) {
+                this.die()
+            }
+        }
+    }
+
+    die() {
+        for (let i = 0; i < predatorArr.length; i++) {
+            if (predatorArr[i].x == this.x && predatorArr[i].y == this.y) {
+                predatorArr.splice(i, 1)
+            }
+        }
+        matrix[this.y][this.x] = 0
+    }
+}
+
